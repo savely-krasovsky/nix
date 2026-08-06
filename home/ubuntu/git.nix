42696fab26331bd76b6d99471104d040ce82fbf0
Savely Krasovsky <savely@krasovs.ky>
2026-08-06 23:04:17 +0200

    feat: enable GPG signing for commits and tags in Git configuration

--- home/ubuntu/git.nix
+++ home/ubuntu/git.nix
@@ -6,10 +6,14 @@
       condition = "gitdir:~/Projects/work/";
       contents = {
         user = {
-          name = "Krasovskiy Saveliy Igorevich";
+          name = "Savely Krasovsky";
           email = "skrasovskiy@ozon.ru";
           signingKey = "3D68247F98B4ADA1B1D852F9720200BABE066085";
         };
+
+        gpg.format = "openpgp";
+        commit.gpgSign = true;
+        tag.gpgSign = true;
       };
     }
   ];
