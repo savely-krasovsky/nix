027cb07176517ec11cea68563ffd2751f87fa681
Savely Krasovsky <savely@krasovs.ky>
2026-08-06 22:50:37 +0200

    feat: update Git signing key for Ubuntu workstation

--- home/ubuntu/git.nix
+++ home/ubuntu/git.nix
@@ -8,7 +8,7 @@
         user = {
           name = "Krasovskiy Saveliy Igorevich";
           email = "skrasovskiy@ozon.ru";
-          signingKey = "A43215817A6E4AF81FF38840A0D6FB316FF65527";
+          signingKey = "3D68247F98B4ADA1B1D852F9720200BABE066085";
         };
       };
     }
